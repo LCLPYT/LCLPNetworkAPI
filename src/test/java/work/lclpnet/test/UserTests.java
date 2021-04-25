a71069862a2013efdbcc101688b7906d2a4e681c
LCLP <lukas@lclpnet.work>
2021-04-25 14:03:10 +0200

    added a dummy unit test

---
+++ src/test/java/work/lclpnet/test/UserTests.java
@@ -0,0 +1,14 @@
+package work.lclpnet.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTests {
+
+    @Test
+    void dummy() {
+        assertEquals(1, 1);
+    }
+
+}
